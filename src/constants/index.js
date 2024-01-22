6fd8dc0f1de328edc8db27a88f32e9f716cf7b3d
Okpube victor <curiousv32@gmail.com>
2024-01-22 03:00:03 -0600

    Updated files

--- src/constants/index.js
+++ src/constants/index.js
@@ -175,7 +175,7 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        "I thought it was impossible to make a website as beautiful as our product, but Victor proved me wrong.",
       name: "Sara Lee",
       designation: "CFO",
       company: "Acme Co",
@@ -183,7 +183,7 @@ import {
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        "After Victor optimized our website, our traffic increased by 50%. We can't thank them enough!",
       name: "Lisa Wang",
       designation: "CTO",
       company: "456 Enterprises",
